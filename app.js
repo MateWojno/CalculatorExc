dc911ef8e5f667c3b2fc13cbf2e3a70f8aefc246
MateWojno <mateusz.k.wojno@gmail.com>
2023-11-07 20:52:21 +0100

    App.js func prototypes, pseudo-code

---
+++ app.js
@@ -0,0 +1,34 @@
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+
+let displayValue = '';
+// input will be string, so parseFloat();
+
+//  target buttons onclick();
+//      get data from buttons,
+//          append firstNum, operator and secondNum on display; 
+//      event listeners;
+
+
+
+function updateDisplay(firstNum, operator, secondNum) {
+
+    // Create the functions that populate the display when you click the number buttons. 
+
+};
+
+function calcResult(num1, operator, num2) {
+    // parse firstNum, secondNum to float 
+    // perform the appropriate operations
+    // use switch 
+};
+
+function clearDisplay(displayValue) {
+    // onclick() "C" button;
+    return displayValue = '';
+};
+
+function keyboardController {
+    // handle keystrokes
+};
