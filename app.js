c33f3af5cc0aae31ef931a805690a7e30c094e4c
MacBookPro <macbookpro@Farzzn.local>
2023-11-11 11:10:38 +0530

    Used experrsion evaluation fuction in JavaScript to evaluate and display result

--- app.js
+++ app.js
@@ -1,59 +1,16 @@
-var firstNum = '';
-var operator = '';
-var secondNum = '';
+let displayValue = '';
 
-const display = document.querySelector(".display");
-
-
-function updateDisplay(input) {
-    var displayValue = display.textContent;
-    if ( 
-        display.textContent.slice(-1) === '*' ||
-        display.textContent.slice(-1) === '/' ||
-        display.textContent.slice(-1) === '+' ||
-        display.textContent.slice(-1) === '-' 
-    ) {
-        //          parse strings to floats;
-        //      if '=' is pressed store num2 and calc result;
-        return valueSetter(displayValue);
-    }
-    display.textContent += input;
-    return displayValue;
-};
-
-function valueSetter(displayValue) {
-    if (    
-        displayValue.slice(-1) === '*' ||
-        displayValue.slice(-1) === '/' ||
-        displayValue.slice(-1) === '+' ||
-        displayValue.slice(-1) === '-' 
-        ) {
-            console.log(displayValue);
-            firstNumArr = displayValue.split(displayValue.slice(-1));
-            firstNum = firstNumArr[0];
-            operator = displayValue.slice(-1);
-            console.log('operator: ', operator, 'firstNum: ', firstNum);
-            return firstNum, operator;
-        }
-    // set secondNum - how?
+function updateDisplay(value) {
+    displayValue += value;
+    document.getElementById('calculatorDisplay').innerText = displayValue;
 }
 
-function calcResult(firstNum, operator, secondNum) {
-   console.log(firstNum, "... operator: ", operator, "...second num: ", secondNum);
-    // parse firstNum, secondNum to float 
-    // perform the appropriate operations
-    // use switch 
-};
-
 function clearDisplay() {
-    firstNum = '';
-    operator = '';
-    secondNum = '';
-    return display.textContent = '';
-};
+    displayValue = '';
+    document.getElementById('calculatorDisplay').innerText = displayValue;
+}
 
-function keyboardController() {
-    // handle keystrokes
-};
-
-
+function calculateResult() {
+    displayValue = eval(displayValue);
+    document.getElementById('calculatorDisplay').innerText = displayValue;
+}
